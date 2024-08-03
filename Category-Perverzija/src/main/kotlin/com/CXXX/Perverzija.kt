24581d70cc0929f0573e5ac9a6670719a4314aba
riojio233 <rohitpradhanjio1379@gmail.com>
2024-08-04 02:52:20 +0530

    ctaegory-perv search change

--- Category-Perverzija/src/main/kotlin/com/CXXX/Perverzija.kt
+++ Category-Perverzija/src/main/kotlin/com/CXXX/Perverzija.kt
@@ -109,7 +109,7 @@ class Perverzija : MainAPI() {
 
     override suspend fun search(query: String): List<SearchResponse> {
         val searchResponse = mutableListOf<SearchResponse>()
-        val maxPages = if (query.contains(" ")) 6 else 20
+        val maxPages = if (query.contains(" ")) 4 else 10
         for (i in 1..maxPages) {
             val url = if (query.contains(" ")) {
             "$mainUrl/page/$i/?s=${query.replace(" ", "+")}&orderby=date"
